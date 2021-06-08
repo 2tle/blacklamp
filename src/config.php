b464f7729d0d69a7faf1cd9cead364a5b57aa45d
양현준 <iam@2tle.io>
2021-06-08 18:17:53 +0900

    Update config.php

--- src/config.php
+++ src/config.php
@@ -3,8 +3,8 @@ require_once 'vendor/autoload.php';
 
 $google_client = new Google_Client();
 $google_client->setApplicationName("Blacklamp");
-$google_client->setClientId('1003920048784-nhd6obcgrjj6mfoebrglav6k1mn6icad.apps.googleusercontent.com');
-$google_client->setClientSecret('9xiMTsyYlCIUfTWB5i984Jtg');
+$google_client->setClientId('');
+$google_client->setClientSecret('');
 $google_client->setRedirectUri('https://blacklamp.lampstudio.xyz/sign');
 $google_client->addScope('email');
 $google_client->addScope('openid');
